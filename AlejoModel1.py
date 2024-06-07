6a5d90933e44242fa895d10c014798e4fc656dd1
jehummel <jehummel@illinois.edu>
2024-06-07 14:03:06 -0500

    File date: 1/18/2019
    Version 3 (1/18/19): Process attended & unattended genuinely in parallel: one iteration for selected for each w/ everything else

--- AlejoModel1.py
+++ AlejoModel1.py
@@ -6,6 +6,8 @@ __author__ = 'john'
 
 # Version 2 (1/17/19): For unattended stimuli, stochastically sample dimensions, with greater weight on relevant (attended) ones
 
+# Version 3 (1/17/19): Process attended & unattended genuinely in parallel: one iteration for selected for each w/ everything else
+
 # last modified 1/17/19
 
 # * * * * * * * * * Graphics bullshit * * * * * * * * *
@@ -57,17 +59,23 @@ BROWN = (155, 30, 0)
 
 # * * * * * * * * * Model-relevant stuff * * * * * * * * *
 
+VERBOSE = False # when true, the model runs only once and tells you events iteration by iteration
+
+# feature dimension indices (1/17/19). Check these against make_feature_vector for consistency
+COLOR_DIMENSIONS = ( 0, 1, 2, 3, 4, 5, 6, 7, 8, 9,10,11,12,13,14,15,16,17) #  0...17 = 18
+SHAPE_DIMENSIONS = (18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34)    # 18...34 = 17
+
+# theory-relevant parameters
+
 TARGET_MATCH_TRESHOLD  = 2 # the threshold an item's integrator must exceed to be matched as the target
 REJECTION_THRESHOLD    = -1.0 # -0.5  # the negative threshold an item's integrator must reach to be rejected from search altogether
 
 EXACT_MATCH_THRESHOLD  = 0.01 # euclidean distance below which two vectors are considered an exact match
 
-# feature dimension indices (1/17/19). Check these against make_feature_vector for consistency
-COLOR_DIMENSIONS = (0,1,2,3,4,5,6,7,8,9,10,11)
-SHAPE_DIMENSIONS = (12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28)
+INTEGRATOR_GUIDED_PRIORITY = 1.0 # [0...1]: degree to which an item's integrator influences it's selection priority: influence means better-matching items are more likely to be selected for evaluation
 
 # for random sampling during inattention
-P_RELEVANT_SAMPLING   = 0.7 # 1.0 # 0.5 # p(sampling) a relevant dimension in unattended processing
+P_RELEVANT_SAMPLING   = 0.95# 0.7 # 1.0 # 0.5 # p(sampling) a relevant dimension in unattended processing
 P_IRRELEVANT_SAMPLING = 0.05 # 0 # 0.05 # p(sampling) an irrelevant dimension in unattended processing
 
 # for feature weighting under selected processing
@@ -76,10 +84,12 @@ IRRELEVANT_WEIGHT = 0.1 # how much irrelevant dimensions contribute to similarit
 
 EXACT_MATCH_COST  = 1 # how many iterations does it take to compute the final, exact match for target verification
 
+ATTENTION_SHIFT_COST = 10 # how many iterations does it cost to switch attention to a new item
+
 class VisualItem(object):
     """
     This is the basic VisualItem data class for the model
-    VisualItems include the target (in the display), the distractors & lires (which are in the display) and the target template (not in the display)
+    VisualItems include the target (in the display), the distractors & lures (which are in the display) and the target template (not in the display)
     """
     def __init__(self,my_list,feature_vector,location=[0,0],name='',is_target=False):
         """
@@ -109,6 +119,9 @@ class VisualItem(object):
         # for search/random selection on iteration-by-iteration basis
         self.priority   = 1.0 # this is a combination of salience, etc. When priority = 0, item has no chance of being selected; self.rejected, priority = 0
         self.subrange   = [0.0,0.0] # selection range: the subrange within [0...1] in which random.random() must fall in order for this guy to be selected
+
+        # DIAG
+        # print 'item created: '+str(self.index)
 
 
 def randomly_select_item(all_items):
@@ -223,17 +236,25 @@ def process_parallel(item,template,relevant):
     # use similarity to update item threshold
     item.integrator += similarity * random.random()
 
+    # 1/18/18: experiment: update priority based on integrator
+    item.priority += item.integrator * INTEGRATOR_GUIDED_PRIORITY
+    if item.priority < 0:
+        item.priority = 0.0
+
     # determine whether rejected
     if item.integrator < REJECTION_THRESHOLD:
         # mark the item as rejected
         item.rejected = True
         item.priority = 0.0
+        if VERBOSE:
+            print str(item.index)+' rejected in parallel phase'
 
 
-def process_item(item,template,relevant,max_iterations=10):
+def process_item(item,template,relevant):
     # this is what happens when an item is randomly selected:
     # it is compared to the target template and its integrator is either incremented or decremented
     #   until threshold crossed or max_iterations spent
+    # Version 3: max_iterations obviated. Now one iteration, but same item stays selected (in run_search) until done
     """
     Compare one item to the target template for num_iterations iterations
     without attention, num_iterations = 1 (Alejo & Simona's "stage 1")
@@ -244,9 +265,9 @@ def process_item(item,template,relevant,max_iterations=10):
     :param relevant: a lst of the dimensions that are relevant for the task
     :return: num_iterations actually processed
     """
-    num_iterations = 0
-    all_done       = False
+
     target_found   = False
+    num_iterations = 1 # will be set to 2 before returning if exact match nees to be checked
 
     # get item/target similarity
     #   (get it before entering loop because it will not change: no need to do each time)
@@ -255,54 +276,66 @@ def process_item(item,template,relevant,max_iterations=10):
     # DIAG
     # print 'similarity = %.3f'%similarity
 
-    # process the item until all_done; then return num_iterations and target_found
-    while not all_done:
-        num_iterations += 1
+    # use similarity to update item threshold
+    item.integrator += similarity
 
-        # use similarity to update item threshold
-        item.integrator += similarity
+    # and compare integrator to thresholds
+    if item.integrator < REJECTION_THRESHOLD:
+        # mark the item as rejected
+        item.rejected = True
+        if VERBOSE:
+            print 'selected item '+str(item.index)+' rejected'
+        item.priority = 0.0
+        item.color    = GRAY
+        item = None # mark item unselected so a new one will get selected next time
 
-        # and compare integrator to thresholds
-        if item.integrator < REJECTION_THRESHOLD:
-            # mark the item as rejected
+    elif item.integrator > TARGET_MATCH_TRESHOLD:
+        # this is very likely a target: double check for exact match
+        if VERBOSE:
+            print 'Selected item '+str(item.index)+' is a serious candidate'
+        target_found = exact_match(item.features,template.features,relevant)
+        # if this was not in fact the target, then mark it rejected
+        if not target_found:
             item.rejected = True
             item.priority = 0.0
-            item.color    = GRAY
-            all_done      = True
+        if VERBOSE:
+            if target_found:
+                print 'Selected item '+str(item.index)+' has been identified as the target!'
+            else:
+                print 'Although a close match, selected item ' + str(item.index) + ' is not the target!'
+        num_iterations += EXACT_MATCH_COST # add an extra iteration cost for this comparison
+        item = None  # mark item unselected so a new one will get selected next time
 
-        elif item.integrator > TARGET_MATCH_TRESHOLD:
-            # this is very likely a target: double check for exact match
-            target_found = exact_match(item.features,template.features,relevant)
-            num_iterations += EXACT_MATCH_COST # add an extra iteration cost for this comparison
 
-        # check to see whether all_done: did we find the target? or is num_iterations >= max_iterations
-        if target_found or num_iterations >= max_iterations:
-            all_done = True
-
-    return (num_iterations, target_found)
+    return (num_iterations, target_found, item)
 
 def make_feature_vector(color,shape):
     # take names for color and shape and make a corresponding feature vector
     # color vectors are [r,r,r,g,g,g,b,b,b,y,y,y]
-    color_vectors = {'red':[1,1,1,-1,-1,-1,0,0,0,0,0,0],    # red = red and Not greem
-                     'green':[-1,-1,-1,1,1,1,0,0,0,0,0,0], # green = green and Not red
-                     'blue':[0,0,0,0,0,0,1,1,1,-1,-1,-1],  # blue = blue & not yellow
-                     'yellow':[0,0,0,0,0,0,-1,-1,-1,1,1,1], # yellow = yellow & blue
-                     'orange':[1,1,0,-1,-1,0,-1,0,0,1,0,0]} # orange is 2 red, 2 not green, 1 yellow, 1 not blue
+    #                          [-------B/W-----][-------R/G------][-------B/Y-------]
+    color_vectors = {'white' :[ 1, 1, 1,-1,-1,-1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                     'black' :[-1,-1,-1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                     'red'   :[ 0, 0, 0, 0, 0, 0, 1, 1, 1,-1,-1,-1, 0, 0, 0, 0, 0, 0],    # red = red and Not green
+                     'green' :[ 0, 0, 0, 0, 0, 0,-1,-1,-1, 1, 1, 1, 0, 0, 0, 0, 0, 0], # green = green and Not red
+                     'blue'  :[ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1,-1,-1,-1],  # blue = blue & not yellow
+                     'yellow':[ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,-1,-1,-1, 1, 1, 1], # yellow = yellow & blue
+                     'orange':[ 0, 0, 0, 0, 0, 0, 1, 1, 0,-1,-1, 0,-1, 0, 0, 1, 0, 0], # orange is 2 red, 2 not green, 1 yellow, 1 not blue
+                     'pink'  :[ 1, 1, 0,-1,-1, 0, 1, 0, 0,-1, 0, 0, 0, 0, 0, 0, 0, 0]}
     # shape is v1,v2,h1,h2,d1,d1,d2,d2,L1,L2,L3,L4,T1,T2,T3,T4,X
-    shape_vectors = {'vertical':[1,1,-1,-1,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                     'horizontal':[-1,-1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                     'T1':[1,0,1,0,0,0,0,0,0,0,0,0,1,0,0,0,0],
-                     'T2':[1,0,1,0,0,0,0,0,0,0,0,0,0,1,0,0,0],
-                     'T3':[1,0,1,0,0,0,0,0,0,0,0,0,0,0,1,0,0],
-                     'T4':[1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1,0],
-                     'L1':[1,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0],
-                     'L2':[1,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,0],
-                     'L3':[1,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0],
-                     'L4':[1,0,1,0,0,0,0,0,0,0,0,1,0,0,0,0,0],
-                     'D1':[0,0,0,0,1,1,-1,-1,0,0,0,0,0,0,0,0],
-                     'D2':[0,0,0,0,-1,-1,1,1,0,0,0,0,0,0,0,0],
-                     'X':[0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,1]}
+    #                             [--V-][--H-][-----D-----][-----L----][-----T----][X]
+    shape_vectors = {'vertical'  :[ 1, 1,-1,-1,-1, 0,-1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                     'horizontal':[-1,-1, 1, 1, 0,-1, 0,-1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                     'T1'        :[ 1, 0, 1, 0,-1, 0,-1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+                     'T2'        :[ 1, 0, 1, 0,-1, 0,-1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+                     'T3'        :[ 1, 0, 1, 0,-1, 0,-1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+                     'T4'        :[ 1, 0, 1, 0,-1, 0,-1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+                     'L1'        :[ 1, 0, 1, 0,-1, 0,-1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+                     'L2'        :[ 1, 0, 1, 0,-1, 0,-1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+                     'L3'        :[ 1, 0, 1, 0,-1, 0,-1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+                     'L4'        :[ 1, 0, 1, 0,-1, 0,-1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+                     'D1'        :[-1, 0,-1, 0, 1, 1,-1,-1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                     'D2'        :[ 0,-1, 0,-1,-1,-1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                     'X'         :[-1, 0, 0,-1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1]}
     color_vector = color_vectors[color]
     shape_vector = shape_vectors[shape]
     feature_vector = []
@@ -312,7 +345,7 @@ def make_feature_vector(color,shape):
     return feature_vector
 
 
-def make_search_items(num,color,shape,display_list,location=[0,0],name = '',is_target=False):
+def make_search_items(num,color,shape,existing_list,location=[0,0],name = '',is_target=False):
     """
     creates a (subset of a) search display: makes num items of color and shape
     num: how many to make
@@ -321,14 +354,14 @@ def make_search_items(num,color,shape,display_list,location=[0,0],name = '',is_t
     disply_list: the larger search display list to which they will be added
     :return: the list of items in the display
     """
-    items = [] # a list of all items to make
+    items = existing_list # a list of all items to make
 
     # get the feature vector
     features = make_feature_vector(color,shape)
 
     # make the required number of these guys
     for i in xrange(num):
-        items.append(VisualItem(display_list,features,location,name,is_target))
+        items.append(VisualItem(items,features,location,name,is_target))
 
     # return 'em
     return items
@@ -347,19 +380,48 @@ def run_search(display, target_template, relevant):
     """
     iteration = 0
     all_done = False # all done with search
+    selected = None  # nothing selected yet; stay on selected until rejected or verified
+
+    # DIAG
+    # print 'entering run_search, Display set ='
+    # for item in display:
+    #     print str(item.index)
+    # print
+    # end DIAG
+
     while not all_done:
         # On Each Iteration...
+        if VERBOSE:
+            print '* * * Iteration '+str(iteration)+' * * *'
 
         # 0) process all in parallel
         for item in display:
-            process_parallel(item,target_template, relevant)
+            if not item.rejected:
+                process_parallel(item,target_template, relevant)
 
         # 1) randomly select one item from the set remaining...
-        selected = randomly_select_item(display)
+        if not selected:
+            selected = randomly_select_item(display)
+            if VERBOSE:
+                print ' item ' + str(selected.index) + ' was just selected'
 
         # 2) evaluate it
         #                                     process_item(item,template,max_iterations)
-        (iteration_increment, target_found) = process_item(selected, target_template, relevant)
+        if not(selected):
+            print 'Caution! Nothing selected!'
+            print 'Your display items:'
+            for item in display:
+                if item.rejected:
+                    rejected_text = '        True'
+                else:
+                    rejected_text = 'False       '
+                print str(item.index)+') '+rejected_text+' %.2f'%item.priority
+            print
+            all_done = True # quit
+            iteration_increment = 0
+            target_found = False
+        else:
+            (iteration_increment, target_found, selected) = process_item(selected, target_template, relevant)
 
         # 2.2) update iteration counter
         iteration += iteration_increment
@@ -378,33 +440,6 @@ def run_search(display, target_template, relevant):
 
     return (iteration, target_found)
 
-
-def simulation1(num_lures):
-    # an easy search: red vertical target among green horizontal distractors
-
-    # 1) make the search template...
-    template = VisualItem(None, make_feature_vector('red', 'vertical'))
-
-    # 1.1) ... and define the relevant dimension(s)
-    relevant = COLOR_DIMENSIONS
-
-    # 2) make the search display
-    search_display = []
-    # 2.1) add the target
-    #                 make_search_items(num,color,shape,display_list,location=[0,0],name = '',is_target=False):
-    search_display.extend(make_search_items(1, 'red', 'vertical', search_display, [0, 0], 'Target', True))
-
-    # 2.2) add the lures: green horizontals
-    search_display.extend(make_search_items(num_lures, 'green', 'horizontal', search_display, [0, 0], 'Target', True))
-
-    # 3) run the search
-    (RT, target_found) = run_search(search_display, template, relevant)
-
-    if not target_found:
-        print 'Error: Target not found'
-
-    # 4) report the results
-    return RT
 
 def mean_and_sem(data):
     # compute the mean and std. error of mean (sem) of the data
@@ -426,18 +461,93 @@ def mean_and_sem(data):
         return None
 
 
+def simulation1(num_lures):
+    # DIAG
+    # print 'entering simulation1()'
+
+    # an easy search: red vertical target among green horizontal distractors
+
+    # 1) make the search template...
+    template = VisualItem(None, make_feature_vector('red', 'vertical'))
+
+    # 1.1) ... and define the relevant dimension(s)
+    relevant = COLOR_DIMENSIONS
+
+    # 2) make the search display
+    search_display = []
+    # 2.1) add the target
+    #                 make_search_items(num,color,shape,display_list,location=[0,0],name = '',is_target=False):
+    search_display = make_search_items(1, 'red', 'vertical', search_display, [0, 0], 'Target', True)
+    # make_search_items(1, 'red', 'vertical', search_display, [0, 0], 'Target', True)
+
+    # 2.2) add the lures: green horizontals
+    search_display = make_search_items(num_lures, 'green', 'vertical', search_display, [0, 0], 'Lure', False)
+
+    # 3) run the search
+    (RT, target_found) = run_search(search_display, template, relevant)
+
+    if not target_found:
+        print 'Error: Target not found'
+
+    # 4) report the results
+    return RT
+
+def simulation2(num_lures):
+    # DIAG
+    # print 'entering simulation2()'
+
+    # a hard search: red vertical target among green vertical and red horizontal distractors
+
+    # 1) make the search template...
+    template = VisualItem(None, make_feature_vector('red', 'vertical'))
+
+    # 1.1) ... and define the relevant dimension(s)
+    relevant = COLOR_DIMENSIONS + SHAPE_DIMENSIONS # this + operation concatenates the vectors
+
+    # 2) make the search display
+    search_display = []
+
+    # 2.1) add the target
+    #                 make_search_items(num,color,shape,display_list,location=[0,0],name = '',is_target=False):
+    search_display = make_search_items(1, 'red', 'vertical', search_display, [0, 0], 'Target', True)
+
+    # 2.2) add the lures:
+
+    # 2.2.1) the number of each Kind of lure is 1/2 the total number of lures
+    num_kind_lures = num_lures/2
+
+    # 2.2.2)green verticals
+    search_display = make_search_items(num_kind_lures, 'green', 'vertical', search_display, [0, 0], 'Lure', False)
+
+    # 2.2.3)red horizontals
+    search_display = make_search_items(num_kind_lures, 'red', 'horizontal', search_display, [0, 0], 'Lure', False)
+
+    # 3) run the search
+    (RT, target_found) = run_search(search_display, template, relevant)
+
+    if not target_found:
+        print 'Error: Target not found'
+
+    # 4) report the results
+    return RT
+
 # * * * * * * * * * * Main Body * * * * * * * * * *
 
 NumRunsPer = 200
 
-lure_nums = (3,6,9,12,15)
+lure_nums = (4,8,12,16,20)
 
 print
 
-for num_lures in lure_nums:
-    data = []
-    for i in xrange(NumRunsPer):
-        data.append(simulation1(num_lures))
-    [mean, sem] = mean_and_sem(data)
-    print str(num_lures)+' easy lures. Mean RT (sem) = %.3f, %.3f'%(mean, sem)
+if VERBOSE:
+    rt = simulation1(8) # run one simulation with 8 lures
+    print 'RT = '+str(rt)
+
+else: # not verbose: run many simulations
+    for num_lures in lure_nums:
+        data = []
+        for i in xrange(NumRunsPer):
+            data.append(simulation1(num_lures))
+        [mean, sem] = mean_and_sem(data)
+        print str(num_lures)+' easy search. Mean RT (sem) = %.3f, %.3f'%(mean, sem)
 
